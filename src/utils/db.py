c7e2d141a91ace7c259549a3053248313a2d2f16
rwiteshbera <rwiteshbera@gmail.com>
2023-09-08 00:36:44 +0530

    fixed type

--- src/utils/db.py
+++ src/utils/db.py
@@ -4,15 +4,17 @@ from rich import print as printC
 from rich.console import Console
 console = Console()
 
+
 def connectDB():
+    db = None
     try:
         db = mysql.connector.connect(
             host='localhost',
             user='admin',
             password='admin'
         )
+
     except Exception as e:
         printC("[red][!] An error occured while trying to connect with vault.[/red]")
 
     return db
-
